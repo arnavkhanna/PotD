8fcf107f2dbb3e34afcda74a979fecf65e3bf128
arnavkhanna <40251972+arnavkhanna@users.noreply.github.com>
2019-06-24 12:09:28 -0700

    Update longestPalindrome.py

--- Longest Palindrome/longestPalindrome.py
+++ Longest Palindrome/longestPalindrome.py
@@ -1,1 +1,13 @@
 
+a = "abcddcaa"
+b = "".join(reversed(a))
+list1 = []
+list2 = []
+for i in b:
+    list2.append(i)
+for i in a:
+    list1.append(i)
+count = 0
+for x in range(len(list1)):
+    y = x+1
+    if list1[y]
