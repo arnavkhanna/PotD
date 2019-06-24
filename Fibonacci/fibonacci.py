d5b7e667861e4a6d9278ec903f00b0c02e08f57f
arnavkhanna <40251972+arnavkhanna@users.noreply.github.com>
2019-06-24 12:07:00 -0700

    Update fibonacci.py

--- Fibonacci/fibonacci.py
+++ Fibonacci/fibonacci.py
@@ -1,1 +1,10 @@
 
+    
+def a(x):
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return a(x-1) + a(x-2)
+
+times1 = int(input("How many numbers do you want to add? "))
+print(a(times1))
